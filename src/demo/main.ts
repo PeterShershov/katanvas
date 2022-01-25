38003f0119eab0cd0f7acdfb5469ddd6695c3d43
Peter Shershov <peters@wix.com>
2022-01-25 23:34:06 +0200

    upgrade deps - add demo for polyline

--- src/demo/main.ts
+++ src/demo/main.ts
@@ -3,11 +3,22 @@ import { Canvas } from '../canvas';
 const canvas = new Canvas();
 canvas.create({ height: 500, width: 500 });
 
-// canvas background;
-canvas.fillCanvas = 'blue';
-// created another rect
-canvas.rect({ x: 60, y: 60, height: 60, width: 60, backgroundColor: 'salmon', strokeColor: '#ccc', lineWidth: 20 });
-canvas.circle({ x: 20, y: 20, radius: 10, backgroundColor: 'gold' });
+canvas.fillCanvas = 'papayawhip';
+
+canvas.rect({ x: 0, y: 0, height: 40, width: 40, backgroundColor: 'salmon', strokeColor: '#ccc', lineWidth: 20 });
+canvas.circle({ x: 50, y: 50, radius: 10, backgroundColor: 'gold' });
+canvas.polyLine({
+    lines: [
+        { x: 0, y: 100 },
+        { x: 80, y: 120 },
+        { x: 0, y: 140 },
+        { x: 80, y: 160 },
+    ],
+    startPosition: { x: 80, y: 80 },
+    lineWidth: 10,
+    strokeColor: 'tomato',
+    backgroundColor: 'gold',
+});
 
 (module as any).hot.dispose(() => {
     canvas.get()?.remove();
